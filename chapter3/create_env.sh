d67af5e903aaf2b3c195fa2c58ea1700c393b331
MutsukiOri <ori.mutsuki@arakawa-lab.com>
2023-06-27 00:33:25 +0900

    fix kaggle.json setting

--- chapter3/create_env.sh
+++ chapter3/create_env.sh
@@ -4,4 +4,4 @@ set -eux
 docker build  -t kaggle-gokui-chapter3:latest .
 
 
-docker run --gpus all -it --init -v .:/home/chapter3 --shm-size=2g --name kaggle-gokui-chapter3 kaggle-gokui-chapter3:latest
+docker run --gpus all -it --init -v .:/home/chapter3 -v ${HOME}/.kaggle:/root/.kaggle --shm-size=2g --name kaggle-gokui-chapter3 kaggle-gokui-chapter3:latest
