1ef3f5505151ed9e7067f3bd00c56af583dbd725
MutsukiOri <ori.mutsuki@arakawa-lab.com>
2023-06-26 22:47:43 +0900

    successfully using gpu

--- chapter3/code/3.4.py
+++ chapter3/code/3.4.py
@@ -8,7 +8,7 @@ import torchvision
 import torchvision.transforms.functional
 from torchvision import transforms
 import os
-
+from tqdm import tqdm
 
 def setup_center_crop_transform():
     return transforms.Compose(
@@ -53,6 +53,102 @@ def setup_train_val_datasets(data_dir, dryrun=False):
 
     return train_dataset, val_dataset
 
+def setup_train_val_loaders(data_dir, batch_size, dryrun=False):
+    train_dataset, val_dataset = setup_train_val_datasets(
+        data_dir, dryrun=dryrun
+    )
+    train_loader = torch.utils.data.DataLoader(
+        train_dataset,
+        batch_size=batch_size,
+        shuffle=True,
+        drop_last=True,
+        num_workers=8,
+    )
+    val_loader = torch.utils.data.DataLoader(
+        val_dataset, batch_size=batch_size, num_workers=8
+    )
+    return train_loader, val_loader
+
+############################################
+# 3.4.2 学習ループ
+############################################
+
+# コード引用あり＠5節
+def train_1epoch(model, train_loader, lossfun, optimizer, device):
+    model.train()
+    total_loss, total_acc = 0.0, 0.0
+
+    for x, y in tqdm(train_loader):
+        x = x.to(device)
+        y = y.to(device)
+
+        optimizer.zero_grad()
+        out = model(x)
+        loss = lossfun(out, y)
+        _, pred = torch.max(out.detach(), 1)
+        loss.backward()
+        optimizer.step()
+
+        total_loss += loss.item() * x.size(0)
+        total_acc += torch.sum(pred == y)
+
+    avg_loss = total_loss / len(train_loader.dataset)
+    avg_acc = total_acc / len(train_loader.dataset)
+    return avg_acc, avg_loss
+
+
+# コード引用あり＠5節
+def validate_1epoch(model, val_loader, lossfun, device):
+    model.eval()
+    total_loss, total_acc = 0.0, 0.0
+
+    with torch.no_grad():
+        for x, y in tqdm(val_loader):
+            x = x.to(device)
+            y = y.to(device)
+
+            out = model(x)
+            loss = lossfun(out.detach(), y)
+            _, pred = torch.max(out, 1)
+
+            total_loss += loss.item() * x.size(0)
+            total_acc += torch.sum(pred == y)
+
+    avg_loss = total_loss / len(val_loader.dataset)
+    avg_acc = total_acc / len(val_loader.dataset)
+    return avg_acc, avg_loss
+
+
+# コード引用あり＠5節
+def train(model, optimizer, train_loader, val_loader, n_epochs, device):
+    lossfun = torch.nn.CrossEntropyLoss()
+
+    for epoch in tqdm(range(n_epochs)):
+        train_acc, train_loss = train_1epoch(
+            model, train_loader, lossfun, optimizer, device
+        )
+        val_acc, val_loss = validate_1epoch(model, val_loader, lossfun, device)
+        print(
+            f"epoch={epoch}, train loss={train_loss}, train accuracy={train_acc}, val loss={val_loss}, val accuracy={val_acc}"
+        )
+
+
+def train_subsec5(data_dir, batch_size, dryrun=False, device="cuda:0"):
+    model = torchvision.models.resnet50(pretrained=True)
+    model.fc = torch.nn.Linear(model.fc.in_features, 2)
+    model.to(device)
+
+    optimizer = torch.optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
+    train_loader, val_loader = setup_train_val_loaders(
+        data_dir, batch_size, dryrun
+    )
+    train(
+        model, optimizer, train_loader, val_loader, n_epochs=1, device=device
+    )
+
+    return model
+
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--data_dir', type=str, default='../dogs_vs_cats')
@@ -65,10 +161,13 @@ def main():
     device = args.device
     dryrun = args.dryrun
 
-    train, val = setup_train_val_datasets(data_dir, dryrun)
+    batch_size = 32
+    train, val = setup_train_val_loaders(data_dir, batch_size, dryrun)
 
-    print(f"train: {len(train)}")
-    print(f"val: {len(val)}")
+    print(len(train.dataset))
+    print(len(val.dataset))
+    model = train_subsec5(data_dir, batch_size, dryrun, device)
+
 
 if __name__ == "__main__":
     main()
--- chapter3/create_env.sh
+++ chapter3/create_env.sh
@@ -4,4 +4,4 @@ set -eux
 docker build  -t kaggle-gokui-chapter3:latest .
 
 
-docker run --gpus all -it --init -v .:/home/chapter3 --env LOCAL_UID=$(id -u $USER) --env LOCAL_GID=$(id -g $USER) --name kaggle-gokui-chapter3 -p 5$(id -u $USER):8888 kaggle-gokui-chapter3:latest
+docker run --gpus all -it --init -v .:/home/chapter3 --memory-reservation="24gb" --shm-size=2g --name kaggle-gokui-chapter3 kaggle-gokui-chapter3:latest
