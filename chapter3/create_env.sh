4d1fdf10a200bf277e9bd2c54f9e817d6f9b0a2c
MutsukiOri <ori.mutsuki@arakawa-lab.com>
2023-06-26 22:52:13 +0900

    fix

--- chapter3/create_env.sh
+++ chapter3/create_env.sh
@@ -4,4 +4,4 @@ set -eux
 docker build  -t kaggle-gokui-chapter3:latest .
 
 
-docker run --gpus all -it --init -v .:/home/chapter3 --memory-reservation="24gb" --shm-size=2g --name kaggle-gokui-chapter3 kaggle-gokui-chapter3:latest
+docker run --gpus all -it --init -v .:/home/chapter3 --shm-size=2g --name kaggle-gokui-chapter3 kaggle-gokui-chapter3:latest
